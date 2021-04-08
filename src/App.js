ca748e0dfec0ce718b3eb6d77fdf06f8aa678931
Tina <longevity.tina@gmail.com>
2021-04-07 21:58:41 -0700

    Sizing, space section 2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,20 @@ function App() {
         </Heading>
       </Hero>
       <SplitSection style={{ height: "526px" }}>
-        <SubSection></SubSection>
+        <SubSection>
+          <SectionDescription>
+            <h1>Lorem Ipsum</h1>
+            <p>
+              Lorem ipsum dolor sit amet, et felis integer. Cras ac, duis nisl
+              magna est sociis, neque in odio vel, sit lobortis erat. Fugit
+              quam, ut pede ut ante, in viverra eros dictum nisl ligula, lacus
+              est vehicula donec. Quam in ac quam. Duis et, non arcu imperdiet
+              sem tellus suspendisse. Condimentum non aliquet sed, quisque risus
+              vitae semper duis feugiat .
+            </p>
+            <a href="/"> > Lorem Ipsum</a>
+          </SectionDescription>
+        </SubSection>
         <SubSection style={{ backgroundImage: `url(${Image1})` }}></SubSection>
       </SplitSection>
       <Section style={{ backgroundImage: `url(${Banner1})` }}></Section>
